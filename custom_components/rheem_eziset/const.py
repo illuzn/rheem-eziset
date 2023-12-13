6e562ff8a9da96a4fcda89b1cbe139b1390b446c
illuzn <william.chau@illuzn.com>
2023-12-13 14:24:27 +0000

    basic reconfiguration

---
+++ custom_components/rheem_eziset/const.py
@@ -0,0 +1,9 @@
+"""Constants for Rheem EziSET."""
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "Rheem EziSET"
+DOMAIN = "rheem_eziset"
+VERSION = "2023.12.0"
+
