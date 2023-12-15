3dc2ba8ae6beb32b09d94eb883ba92f3d1c178fe
illuzn <william.chau@illuzn.com>
2023-12-15 05:06:27 +0000

    bump version

--- custom_components/rheem_eziset/const.py
+++ custom_components/rheem_eziset/const.py
@@ -10,7 +10,7 @@ NAME = "Rheem EziSET Water Heater"
 IDPREFIX = "rheem_water_heater_"
 DOMAIN = "rheem_eziset"
 MANUFACTURER = "Rheem"
-VERSION = "2023.12.0"
+VERSION = "2023.12.1"
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
     Platform.BINARY_SENSOR,
